c5ea51c3e5336b499bc7fb04454b4b95e790d0e7
sandburman <sand.burman@gmail.com>
2016-12-23 21:33:57 +1300

    Eclipse Modification

---
+++ QuickStart/src/main/java/com/sandburman/obj/Blog.java
@@ -0,0 +1,35 @@
+package com.sandburman.obj;
+
+public class Blog {
+  private long id;
+  private String name;
+  private String content;
+
+  public void Blog() {
+  }
+
+  public void setId (long id) {
+    this.id = id;
+  }
+
+  public long getId () {
+    return this.id;
+  }
+
+  public void setName (String id) {
+    this.name = name;
+  }
+
+  public String getName () {
+    return this.name;
+  }
+
+  public void setContent (String id) {
+    this.content = content;
+  }
+
+  public String getContent () {
+    return this.content;
+  }
+
+}
--- QuickStart/src/main/java/com/sandburman/quickstart/App.java
+++ QuickStart/src/main/java/com/sandburman/quickstart/App.java
@@ -1,13 +1,55 @@
 package com.sandburman.quickstart;
 
+import java.util.*;
+import static java.util.stream.Collectors.*;
+import java.sql.*;
+
+import com.sandburman.obj.*;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // List<String> names = Arrays.asList("Tom", "Jerry", "Jack", "Jill");
+        //
+        // System.out.println( "Hello World!" );
+        //
+        // System.out.println(
+        //     names.stream()
+        //          .map(String::toUpperCase)
+        //          .collect(joining(", "))
+        // );
+
+    }
+
+    static {
+      try {
+        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    private static Connection getConnection() throws SQLException {
+      return DriverManager.getConnection("jdbc:derby://localhost:1527/MyDB", "sandeep", "@Dolby786");
+    }
+
+    private static void persistBlog (Blog blog) {
+      String query = "INSERT INTO BLOG (ID, NAME, CONTENT) VALUES (?,?,?)";
+
+      try {
+        PreparedStatement stmt = getConnection().prepareStatement(query);
+        stmt.setLong(1, blog.getId());
+        stmt.setString(1, blog.getName());
+        stmt.setString(1, blog.getContent());
+
+        stmt.executeUpdate();
+
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 }
